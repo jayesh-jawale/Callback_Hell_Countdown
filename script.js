235a1e54c0f4da99ce1d7984f376a18b1a00162b
Jayesh Jawale <86474320+jayesh-jawale@users.noreply.github.com>
2021-09-08 23:42:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var container = document.querySelector('.container');
+
+setTimeout(() => {
+    container.innerHTML = '<h1>10</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>9</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>8</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>7</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>6</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>5</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>4</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>3</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>2</h1>';
+setTimeout(() => {
+    container.innerHTML = '<h1>1</h1>';
+setTimeout(() => {
+    container.innerHTML =   `<h1>
+                            <span class="saffron">Happy</span> <br>
+                            <span class="white">Independence</span> <br>
+                            <span class="green">Day</span>
+                            </h1>`                      
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
+}, 1000)
